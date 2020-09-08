89a447e60b416fd6e005ecfda80ec87c79b2aabe
amarjeetprasad <56919207+amarjeetprasad@users.noreply.github.com>
2020-09-08 23:35:13 +0530

    new update

---
+++ build/precache-manifest.ef4642a81ddcd408219cc8b6de0b5990.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8d44faba0aa4a7096d0bbfc9a4bf29",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "7ebdaf7f2707240a6889",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "159d84ceb72a4a83f1e6",
+    "url": "/Robots/static/css/main.49030957.chunk.css"
+  },
+  {
+    "revision": "7ebdaf7f2707240a6889",
+    "url": "/Robots/static/js/2.64d0f395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.64d0f395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159d84ceb72a4a83f1e6",
+    "url": "/Robots/static/js/main.6f14bf0c.chunk.js"
+  },
+  {
+    "revision": "f756081b821c21819cc9",
+    "url": "/Robots/static/js/runtime-main.a416d013.js"
+  }
+]);
